85af7cb139c9570dbb28320c3bc83fd3f81f7c3e
Arpit Gupta <www.arpitgupta64@gmail.com>
2025-05-22 01:05:03 +0530

    Create Leetcode_3024.java

---
+++ Leetcode_3024.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        int i = 0;
+        int j = i+1;
+        int k = i+2;
+         if(nums[i]+nums[j] <= nums[k] || nums[j]+nums[k] <= nums[i] || nums[i]+nums[k]   <=nums[j]) {
+              return "none";
+         }
+        if(nums[i]==nums[j] && nums[j]==nums[k]) 
+            return "equilateral";
+         else if(nums[i]==nums[j] || nums[i]==nums[k] || nums[j]==nums[k]) 
+            return "isosceles";
+         else
+            if(nums[i]+nums[j] > nums[k] && nums[j]+nums[k] > nums[i] && nums[i]+nums[k]         > nums[j]) 
+              return "scalene";  
+            else 
+            return "none";
+    }
+}
