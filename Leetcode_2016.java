d3dd756df882b1e0a5a6073394e13a445e677089
Arpit Gupta <www.arpitgupta64@gmail.com>
2025-06-16 13:56:57 +0530

    Maximum Difference Between Increasing Elements

---
+++ Leetcode_2016.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int n = nums.length;
+        int minVal = nums[0];
+        int maxDiff = -1;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > minVal) {
+                maxDiff = Math.max(maxDiff, nums[i]-minVal);
+            } else {
+                minVal = nums[i];
+            }
+        }
+        return maxDiff;
+    }
+}
